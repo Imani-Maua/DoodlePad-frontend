f6aa19aa17a359ab24dd656e663a081d2341631e
Imani-Maua <elainewalter7022@gmail.com>
2026-02-13 21:10:59 +0100

    Created an about page

---
+++ src/pages/About.jsx
@@ -0,0 +1,10 @@
+function About(){
+    return(
+        <div>
+            <h1>This is the About Page</h1>
+            <p>This is a note-taking app!</p>
+        </div>
+    );
+}
+
+export default About;

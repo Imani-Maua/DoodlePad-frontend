079361b2ad5790d251bffe3a15e8af0d7a5d72ff
Imani-Maua <elainewalter7022@gmail.com>
2026-02-13 21:10:05 +0100

    Created and implemented light/dark theme context

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,35 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
+import { ThemeProvider } from './context/ThemeContext';
 import PrivateRoute from './components/PrivateRoute';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
 import Dashboard from './pages/Dashboard';
+import About from './pages/About';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <AuthProvider>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Navigate to="/dashboard" replace />} />
-          {/**<Route path="/login" element={<Login />} />*/}
-          <Route path="/signup" element={<Signup />} />
-          <Route
-            path="/dashboard"
-            element={
-              <PrivateRoute>
+      <ThemeProvider>
+        <Router>
+          <div>
+            <Navbar />
+          </div>
+          <Routes>
+            <Route path="/" element={<Navigate to="/dashboard" replace />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path='/about' element={<About />} />
+            <Route
+              path="/dashboard"
+              element={
                 <Dashboard />
-              </PrivateRoute>
-            }
-          />
-        </Routes>
-      </Router>
+              }
+            />
+          </Routes>
+        </Router>
+      </ThemeProvider>
     </AuthProvider>
   );
 }

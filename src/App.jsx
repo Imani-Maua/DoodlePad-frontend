dcc9c2e212e8f553372a8d94cf556db4c6f446dc
Imani-Maua <elainewalter7022@gmail.com>
2026-02-19 08:04:06 +0100

    Fixed bugs

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
-import { ThemeProvider } from './context/ThemeContext';
 import { NotificationProvider } from './context/NotificationContext';
 import PrivateRoute from './components/PrivateRoute';
 import Login from './pages/Login';
@@ -12,27 +11,25 @@ import NotificationDisplay from './components/NotificationDisplay';
 function App() {
   return (
     <AuthProvider>
-      <ThemeProvider>
-        <NotificationProvider>
-          <Router>
-            <NotificationDisplay />
-            <Navbar />
-            <Routes>
-              <Route path="/" element={<Navigate to="/dashboard" replace />} />
-              <Route path="/login" element={<Login />} />
-              <Route path="/signup" element={<Signup />} />
-              <Route
-                path="/dashboard"
-                element={
-                  <PrivateRoute>
-                    <Dashboard />
-                  </PrivateRoute>
-                }
-              />
-            </Routes>
-          </Router>
-        </NotificationProvider>
-      </ThemeProvider>
+      <NotificationProvider>
+        <Router>
+          <NotificationDisplay />
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Navigate to="/dashboard" replace />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route
+              path="/dashboard"
+              element={
+                <PrivateRoute>
+                  <Dashboard />
+                </PrivateRoute>
+              }
+            />
+          </Routes>
+        </Router>
+      </NotificationProvider>
     </AuthProvider>
   );
 }
